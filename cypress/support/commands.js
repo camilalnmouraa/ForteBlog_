7ebc05283f168ce48aa852c7ffa3643c698cc0d6
Camila Moura <124525550+camilalnmouraa@users.noreply.github.com>
2024-01-20 21:33:03 -0300

    Delete cypress directory

--- cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-import loc from '../support/locators'
-
-Cypress.Commands.add('home', () => {
-    cy.visit('https://qastoredesafio.lojaintegrada.com.br/')
-    cy.title().should('eq', 'QA Store Desafio')
-
-})
